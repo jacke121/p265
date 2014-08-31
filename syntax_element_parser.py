79e3719432722a7a21f161da21834eca812fbfa7
brxue <brxue@outlook.com>
2014-09-01 00:31:13 +0800

    Add SPS (partial)

--- syntax_element_parser.py
+++ syntax_element_parser.py
@@ -130,6 +130,41 @@ class Sps:
         self.profile_tier_level = ProfileTierLevel(bs)
 
     def parse(self):
+        self.sps_video_parameter_set_id = self.bs.u(4, "sps_video_parameter_set_id")
+        self.sps_max_sub_layers_minus1 = self.bs.u(3, "sps_max_sub_layers_minus1")
+        self.sps_temporal_id_nesting_flag = self.bs.u(1, "sps_temporal_id_nesting_flag")
+
+        self.profile_tier_level.parse(self.sps_max_sub_layers_minus1)
+
+        self.sps_seq_parameter_set_id = self.bs.ue("sps_seq_parameter_set_id")
+        self.chroma_format_idc = self.bs.ue("chroma_format_idc")
+        if self.chroma_format_idc == 3:
+            self.seperate_colour_plane_flag = self.bs.u(1, "seperate_colour_plane_flag")
+
+        self.pic_width_in_luma_samples = self.bs.ue("pic_width_in_luma_samples")
+        self.pic_height_in_luma_samples = self.bs.ue("pic_height_in_luma_samples")
+
+        self.conformance_window_flag = self.bs.u(1, "conformance_window_flag")
+        if self.conformance_window_flag:
+            self.conf_win_left_offset = self.bs.ue("conf_win_left_offset")
+            self.conf_win_right_offset = self.bs.ue("conf_win_right_offset")
+            self.conf_win_top_offset = self.bs.ue("conf_win_top_offset")
+            self.conf_win_bottom_offset = self.bs.ue("conf_win_bottom_offset")
+
+        self.bit_depth_luma_minus8 = self.bs.ue("bit_depth_luma_minus8")
+        self.bit_depth_chroma_minus8 = self.bs.ue("bit_depth_chroma_minus8")
+
+        self.log2_max_pic_order_cont_lsb_minus4 = self.bs.ue("log2_max_pic_order_cont_lsb_minus4")
+        self.sps_sub_layer_ordering_info_present_flag = self.bs.u(1, "sps_sub_layer_ordering_info_present_flag")
+        
+        self.sps_max_dec_pic_buffering_minus1 = [0] * (self.sps_max_sub_layers_minus1+1)
+        self.sps_max_num_reorder_pics = [0] * (self.sps_max_sub_layers_minus1+1)
+        self.sps_max_latency_increase_plus1 = [0] * (self.sps_max_sub_layers_minus1+1)
+        for i in range(0 if self.sps_sub_layer_ordering_info_present_flag else self.sps_max_sub_layers_minus1, self.sps_max_sub_layers_minus1+1):
+            self.sps_max_dec_pic_buffering_minus1[i] = self.bs.ue("sps_max_dec_pic_buffering_minus1")
+            self.sps_max_num_reorder_pics[i] = self.bs.ue("sps_max_num_reorder_pics")
+            self.sps_max_latency_increase_plus1[i] = self.bs.ue("sps_max_latency_increase_plus1")
+
         raise "TODO @ SPS"
 
 class NaluHeader:
@@ -140,7 +175,6 @@ class NaluHeader:
         print >>self.bs.log, "============= NALU Header ============="
         self.forbidden_zero_bit = self.bs.u(1, "forbidden_zero_bit")
         assert(self.forbidden_zero_bit == 0)
-        print "byte_idx = %d bit_idx = %d" % (bs.byte_idx, bs.bit_idx)
         self.nal_unit_type = self.bs.u(6, "nal_unit_type")
         self.nuh_layer_id = self.bs.u(6, "nuh_layer_id")
         self.nuh_temporal_id_plus1 = self.bs.u(3, "nuh_temporal_id_plus1")
@@ -160,14 +194,17 @@ class AnnexB:
     def parse(self):
         while True:
             self.bs.search_start_code()
+            self.bs.report_position()
 
             self.nalu.naluh.parse()
             nalu_type = self.nalu.naluh.nal_unit_type
 
             if nalu_type == 32:
                 self.nalu.vps.parse()
+                self.bs.report_position()
             elif nalu_type == 33:
                 self.nalu.sps.parse()
+                self.bs.report_position()
                 raise "TODO"
             else:
                 raise "Error: unimplemeted NALU type."
