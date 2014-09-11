ba315bb635af6f3ab1536a8ef91545db04984ee7
brxue <brxue@outlook.com>
2014-09-12 00:06:30 +0800

    add check_ctb_availability()

--- slice.py
+++ slice.py
@@ -176,6 +176,7 @@ class SliceData:
         self.slice_header = slice_header
         self.img = img
         self.cabac = cabac.Cabac(bs)
+        self.ctbs = [{}] * 2048 # TODO make the size precise
 
     def decode(self):
         if not self.slice_header.dependent_slice_segment_flag:
@@ -186,7 +187,61 @@ class SliceData:
             raise "Intentional Stop."
 
     def parse_coding_tree_unit(self):
+        self.ctb_addr_rs = self.slice_header.slice_segment_address #TODO: true if first ctb of slice
+
+        self.ctb[self.ctb_addr_rs]["slice_addr"] = [self.slice_header.slice_segment_address]
+
+        self.x_ctb = self.ctb_addr_rs % self.sps.pic_width_in_ctbs_y
+        self.y_ctb = self.ctb_addr_rs / self.sps.pic_width_in_ctbs_y
+
+        self.x_ctb_pixels = self.x_ctb << self.sps.ctb_log2_size_y
+        self.y_ctb_pixels = self.y_ctb << self.sps.ctb_log2_size_y
+
+        if self.slice_header.slice_sao_luma_flag or self.slice_header.slice_sao_chroma_flag:
+            raise "Unsupported yet."
+        
+        self.parse_coding_quadtree(self.x_ctb_pixels, self.y_ctb_pixels, self.sps.ctb_log2_size_y, 0)
+
+        # should return a CTU
+
+    def parse_coding_quadtree(self, x0, y0, log2_ctb_size, cqt_depth):
+        if (x0 + (1 << log2_ctb_size)) <= self.sps.pic_width_in_luma_samples and \
+           (y0 + (1 << log2_ctb_size)) <= self.sps.pic_height_in_luma_samples and \
+           log2_ctb_size > self.sps.min_cb_log2_size_y:
+            split_cu_flag
+
         pass
+
+    def get_ctb_addr_rs_from_luma_position(self, x, y):
+        ctb_x = x >> self.sps.log2_ctb_size_y;
+        ctb_y = y >> self.sps.log2_ctb_size_y;
+    
+        return ctb_y * self.sps.pic_width_in_ctbs_y + ctb_x;
+
+    def get_slice_addr_rs_from_ctb_addr_rs(self, ctb_addr_rs):
+        return self.ctbs[ctb_addr_rs]["slice_addr"]
+
+    def check_ctb_availability(self, x_current, y_current, x_neighbor, y_neighbor):
+        if x_neighbor < 0 or y_neighbor < 0: return 0
+        
+        if x_neighbor >= self.sps.pic_width_in_luma_samples:  return 0
+        if y_neighbor >= self.sps.pic_height_in_luma_samples: return 0
+
+        current_ctb_addr_rs  = self.get_ctb_addr_rs_from_luma_position(x_current, y_current);
+        neighbor_ctb_addr_rs = self.get_ctb_addr_rs_from_luma_position(x_neighbor, y_neighbor);
+
+        # TODO: check if this is correct (6.4.1)
+        if self.get_slice_addr_rs_from_ctb_addr_rs(self, current_ctb_addr_rs) !=\
+           self.get_slice_addr_rs_from_ctb_addr_rs(self, neighbor_ctb_addr_rs):
+            return 0
+
+        # check if both CTBs are in the same tile.
+        if (img->pps.TileIdRS[current_ctbAddrRS] !=
+            img->pps.TileIdRS[neighbor_ctbAddrRS]) {
+            return 0;
+        }
+
+        return 1
 
 class SliceSegment:
     def __init__(self, bs, naluh, vps, sps, pps, img):
