635cd9ceadc647c6df31a5628c387321bc481d5a
brxue <brxue@outlook.com>
2014-09-14 14:34:21 +0800

    initial submit of sao decoding

--- slice.py
+++ slice.py
@@ -21,28 +21,92 @@ class SliceData:
         if not self.slice_header.dependent_slice_segment_flag:
             self.cabac.initialize_context_models(self.slice_header)
 
-        self.ctb_addr_rs = self.slice_header.slice_segment_address
+        self.ctb_addr_in_raster_scan = self.slice_header.slice_segment_address
 
         while True:
             self.parse_coding_tree_unit()
             raise "Intentional Stop."
 
     def parse_coding_tree_unit(self):
-        self.x_ctb = self.ctb_addr_rs % self.sps.pic_width_in_ctbs_y
-        self.y_ctb = self.ctb_addr_rs / self.sps.pic_width_in_ctbs_y
 
-        self.x_ctb_pixels = self.x_ctb << self.sps.ctb_log2_size_y
-        self.y_ctb_pixels = self.y_ctb << self.sps.ctb_log2_size_y
-
-        self.ctb = ctb.Ctb(self.x_ctb_pixels, self.y_ctb_pixels, self.sps)
+        self.ctb = ctb.Ctb(self.ctb_addr_in_raster_scan, self.sps)
         self.ctb.slice_addr = self.slice_header.slice_segment_address
 
         if self.slice_header.slice_sao_luma_flag or self.slice_header.slice_sao_chroma_flag:
-            raise "Unsupported yet."
+            self.decode_sao()
         
-        self.parse_coding_quadtree(self.x_ctb_pixels, self.y_ctb_pixels, self.sps.ctb_log2_size_y, 0)
+        self.parse_coding_quadtree(self.ctb.x_ctb, self.y_ctb, self.sps.ctb_log2_size_y, 0)
 
         # should return a CTU
+
+    def decode_sao_merge_left_flag(self):
+        pass
+
+    def decode_sao_merge_up_flag(self):
+        pass
+
+    def decode_sao_type_idx_luma(self):
+        pass
+
+    def decode_sao_type_idx_chroma(self):
+        pass
+    
+    def decode_sao_offset_abs(self):
+        pass
+    
+    def decode_sao_offset_sign(self):
+        pass
+
+    def decode_sao(self):
+        self.sao_merge_left_flag = 0
+        if self.ctb.x_ctb > 0:
+            left_ctb_in_slice_seg = self.ctb.addr > self.ctb.slice_addr
+            left_ctb_in_tile =  self.pps.tile_id[self.ctb.addr_in_tile_scan] == \
+                    self.pps.tile_id[self.pps.ctb_addr_rs2ts[self.ctb.addr_in_raster_scan-1]]
+            if left_ctb_in_slice_seg and left_ctb_in_tile:
+                self.sao_merge_left_flag = self.decode_sao_merge_left_flag()
+
+        self.sao_merge_up_flag = 0
+        if self.ctb.y_ctb > 0 and (not self.sao_merge_left_flag):
+            up_ctb_in_slice_seg = (self.ctb.addr - self.sps.pic_width_in_ctbs_y) >= self.ctb.slice_addr
+            up_ctb_in_tile = self.pps.tile_id[self.ctb.addr_in_tile_scan] == \
+                    self.tile_id[self.pps.ctb_addr_rs2ts[self.ctb.addr_in_raster_scan - self.sps.pic_width_in_ctbs_y]]
+            
+            if up_ctb_in_slice_seg and up_ctb_in_tile:
+                self.sao_merge_up_flag = self.decode_sao_merge_up_flag()
+        
+        sao_info = ctb.SaoInfo()
+        if (not self.sao_merge_up_flag) and (not self.sao_merge_left_flag):
+            for colorIdx in range(3):
+                if (self.slice_hdr.slice_sao_luma_flag and colorIdx==0) or (self.slice_hdr.slice_sao_chroma_flag and colorIdx>0)
+                    if colorIdx == 0:
+                        self.sao_type_idx_luma == self.decode_sao_type_idx_luma()
+                        sao_info.sao_type_idx[0] = self.sao_type_idx_luma
+                    elif colorIdx == 1:
+                        self.sao_type_idx_chroma == self.decode_sao_type_idx_chroma()
+                        sao_info.sao_type_idx[1] = self.sao_type_idx_chroma
+                        sao_info.sao_type_idx[2] = self.sao_type_idx_chroma
+                   
+                    if sao_info.sao_type_idx[colorIdx] != 0:
+                        self.sao_offset_abs[colorIdx] = [0] * 4
+                        for i in range(4):
+                            self.sao_offset_abs[colorIdx][i] = self.decode_sao_offset_abs()
+
+                        if sao_info.sao_type_idx[colorIdx] == 1:
+                            for i in range(4):
+                                if self.sao_offset_abs[colorIdx][i] != 0:
+                                    self.sao_offset_sign[colorIdx][i] = self.decode_sao_offset_sign()
+        else:
+            for colorIdx in range(3):
+                if self.sao_merge_left_flag:
+                    sao_info.sao_type_idx[colorIdx] = self.ctbs[self.ctb.x_ctb-1][self.ctb.y_ctb].sao_info.sao_tpe_idx[colorIdx]
+                elif self.sao_merge_up_flag:
+                    sao_info.sao_type_idx[colorIdx] = self.ctbs[self.ctb.x_ctb][self.ctb.y_ctb-1].sao_info.sao_tpe_idx[colorIdx]
+                else:
+                    sao_info.sao_type_idx[colorIdx] = 0
+
+            #TODO:add default values for sao_offset_abs and sao_offset_sign
+
 
     def parse_coding_quadtree(self, x0, y0, log2_ctb_size, cqt_depth):
         if (x0 + (1 << log2_ctb_size)) <= self.sps.pic_width_in_luma_samples and \
@@ -50,7 +114,7 @@ class SliceData:
            log2_ctb_size > self.sps.min_cb_log2_size_y:
             split_cu_flag
 
-        pass
+        raise "Unimplemented yet." 
 
     def get_ctb_addr(self, x, y):
         ctb_x = x >> self.sps.log2_ctb_size_y;
