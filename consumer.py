e0f133bb372438d088baec26ffb0846efd3761dc
brxue <brxue@outlook.com>
2014-09-26 21:36:50 +0800

    Remove unused files

--- consumer.py
+++
@@ -1,7 +0,0 @@
-import threading
-
-class Consumer(threading.Thread):
-    def __init__(self, q):
-        super(Consumer, self).__init__()
-        self.fifo = q
-
--- producer.py
+++
@@ -1,6 +0,0 @@
-import threading
-
-class Producer(threading.Thread):
-    def __init__(self, q):
-        super(Producer, self).__init__()
-        self.fifo =  q
